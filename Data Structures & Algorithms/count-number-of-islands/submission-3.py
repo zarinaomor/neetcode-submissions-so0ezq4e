ddad318aafd65c6a2dcaa869d2cc0111be89d062
zarinaomor <zarinaomorova@yahoo.com>
2026-06-03 15:37:44 -0700

    Add: count-number-of-islands - submission-3

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-3.py
@@ -0,0 +1,76 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+        islands = 0
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+        def dfs(r, c):
+            if r < 0 or c < 0 or r == ROWS or c == COLS or grid[r][c] == '0' or (r, c) in visit:
+                return 
+
+            visit.add((r, c))
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == '1' and (r, c) not in visit:
+                    dfs(r, c)
+                    islands += 1
+        return islands
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # ROWS, COLS = len(grid), len(grid[0])
+        # island = 0
+        # visit = set()
+
+        # def dfs(r, c):
+        #     if r < 0 or c < 0 or r == ROWS or c == COLS or (r, c) in visit or grid[r][c] == '0':
+        #         return 0
+
+        #     visit.add((r, c))
+
+        #     dfs(r + 1, c)
+        #     dfs(r - 1, c)
+        #     dfs(r, c + 1)
+        #     dfs(r, c - 1)
+
+          
+        
+            
+        # for r in range(ROWS):
+        #     for c in range(COLS):
+        #         if grid[r][c] == '1' and (r, c) not in visit:
+        #             island += 1
+        #             dfs(r, c)
+
+        # return island
+
+        
+
+
+
+
+
+
+
